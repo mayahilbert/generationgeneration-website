4b95b8a33d76af4bdcd1473a0077f3d10aae9d17
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2022-11-30 22:18:25 -0500

    new project template

--- assets/js/main.js
+++ assets/js/main.js
@@ -217,3 +217,20 @@
 			});
 
 })(jQuery);
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+	autoHeight: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
